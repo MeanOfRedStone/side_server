6db2b8064951f1685707079cde312612eb5d9b89
MeanOfRedStone <hongs7701@naver.com>
2025-03-16 01:22:21 +0900

    [ADD] ItemDto 객체 추가
    Item 로직을 검증하기 위한 데이터를 전송할 때 사용하는 Dto

---
+++ side/src/main/java/com/server/side/item/dto/ItemDto.java
@@ -0,0 +1,38 @@
+package com.server.side.item.dto;
+
+import com.server.side.item.domain.Item;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class ItemDto {
+
+    private long id;
+
+    private String name;
+
+    private int price;
+
+    private String category;
+
+    private String image;
+
+    private String information;
+
+    private String measurement;
+
+    public static ItemDto fromEntity(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .price(item.getPrice())
+                .category(item.getCategory())
+                .image(item.getImage())
+                .information(item.getInformation())
+                .measurement(item.getMeasurement())
+                .build();
+    }
+}
