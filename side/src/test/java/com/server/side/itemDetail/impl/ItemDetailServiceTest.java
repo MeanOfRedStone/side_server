784d3fe538f9edb9a0ee4ec0c739d37bfe7a5bb8
홍석의 <hongs7701@naver.com>
2025-03-17 02:16:44 +0900

    [TEST] addItemDetail 테스트 구현
    request와 같은 값의 객체를 저장하는 지 확인

---
+++ side/src/test/java/com/server/side/itemDetail/impl/ItemDetailServiceTest.java
@@ -0,0 +1,65 @@
+package com.server.side.itemDetail.impl;
+
+import com.server.side.item.domain.Item;
+import com.server.side.itemDetail.domain.ItemDetail;
+import com.server.side.itemDetail.domain.ItemDetailRepository;
+import com.server.side.itemDetail.dto.ItemDetailRegistrationRequest;
+import com.server.side.itemDetail.service.impl.ItemDetailServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.server.side.itemDetail.dto.ItemDetailDTO.fromEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemDetailServiceTest {
+
+    @InjectMocks
+    ItemDetailServiceImpl itemDetailService;
+    @Mock
+    ItemDetailRepository itemDetailRepository;
+
+    @Test
+    void addItemDetailThenReturnSame() {
+        Item item = Item.builder()
+                .id(1L)
+                .name("셔츠")
+                .price(1000)
+                .category("상의")
+                .image("셔츠.png")
+                .information("멋진 셔츠")
+                .measurement("L - 100")
+                .build();
+        ItemDetailRegistrationRequest request1 = ItemDetailRegistrationRequest.builder()
+                                                                            .item(item)
+                                                                            .option("L")
+                                                                            .quantity(100)
+                                                                            .build();
+
+        given(itemDetailRepository.save(any(ItemDetail.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        assertEquals(fromEntity(request1.toEntity()), itemDetailService.addItemDetail(request1));
+
+        verify(itemDetailRepository, times(1)).save(request1.toEntity());
+
+        ItemDetailRegistrationRequest request2 = ItemDetailRegistrationRequest.builder()
+                .item(item)
+                .option("M")
+                .quantity(50)
+                .build();
+
+        assertEquals(fromEntity(request2.toEntity()), itemDetailService.addItemDetail(request2));
+
+        verify(itemDetailRepository, times(1)).save(request2.toEntity());
+    }
+
+
+}
