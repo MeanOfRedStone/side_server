45b3817723b34a2c5bde11ef9b54471b861558eb
meanofredstone <hongs7701@naver.com>
2025-03-20 14:48:44 +0900

    [TESt] Item addItemTest 추가
    Request 객체와 같은 값을 갖는 객체를 저장하여 ItemDTO로 반환

--- side/src/test/java/com/server/side/item/service/impl/ItemServiceImplTest.java
+++ side/src/test/java/com/server/side/item/service/impl/ItemServiceImplTest.java
@@ -1,4 +1,57 @@
 package com.server.side.item.service.impl;
 
+import com.server.side.item.domain.Item;
+import com.server.side.item.domain.ItemRepository;
+import com.server.side.item.dto.ItemRegistrationRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.server.side.item.dto.ItemDto.fromEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class ItemServiceImplTest {
+
+    @InjectMocks
+    ItemServiceImpl itemService;
+    @Mock
+    ItemRepository itemRepository;
+
+    @Test
+    void addItemThenReturnSame() {
+        ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
+                .name("셔츠")
+                .price(1000)
+                .category("상의")
+                .image("셔츠.png")
+                .information("멋진 셔츠")
+                .measurement("L - 100")
+                .build();
+
+        given(itemRepository.save(any(Item.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        assertEquals(fromEntity(request1.toEntity()), itemService.addItem(request1));
+        verify(itemRepository, times(1)).save(request1.toEntity());
+
+        ItemRegistrationRequest request2 = ItemRegistrationRequest.builder()
+                .name("청바지")
+                .price(2000)
+                .category("하의")
+                .image("청바지.png")
+                .information("멋진 청바지")
+                .measurement("XL - 32")
+                .build();
+
+        assertEquals(fromEntity(request2.toEntity()), itemService.addItem(request2));
+        verify(itemRepository, times(1)).save(request2.toEntity());
+
+    }
 }
