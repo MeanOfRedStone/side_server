6bbb78d1e01afed97a2c5ac57f5435088a5374a5
meanofredstone <hongs7701@naver.com>
2025-03-21 00:18:46 +0900

    [TEST] createItem 테스트 추가
    createItem()의 api주소로 데이터를 추가하면 같은 객체를 반환하는지 확인

---
+++ side/src/test/java/com/server/side/item/controller/ItemControllerTest.java
@@ -0,0 +1,64 @@
+package com.server.side.item.controller;
+
+import com.google.gson.Gson;
+import com.server.side.item.dto.ItemDto;
+import com.server.side.item.dto.ItemRegistrationRequest;
+import com.server.side.item.service.impl.ItemServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ItemServiceImpl itemService;
+
+    @Autowired
+    private Gson gson;
+
+    @Test
+    void createItemThenReturnSameItem() throws Exception {
+        ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
+                .name("셔츠")
+                .price(1000)
+                .category("상의")
+                .image("셔츠.png")
+                .information("멋진 셔츠")
+                .measurement("L - 100")
+                .build();
+        ItemDto dto1 = request1.toResponse();
+
+        given(itemService.addItem(eq(request1)))
+                .willReturn(request1.toEntity());
+
+        ResultActions actions = mockMvc.perform(
+                post("/items")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(request1))
+        );
+
+        actions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(dto1.getName()))
+                .andExpect(jsonPath("$.price").value(dto1.getPrice()))
+                .andExpect(jsonPath("$.category").value(dto1.getCategory()))
+                .andExpect(jsonPath("$.image").value(dto1.getImage()))
+                .andExpect(jsonPath("$.information").value(dto1.getInformation()))
+                .andExpect(jsonPath("$.measurement").value(dto1.getMeasurement()));
+    }
+}
