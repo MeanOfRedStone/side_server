3ef7f640f37142dc66073cd645df4d9f58fb8df6
meanofredstone <hongs7701@naver.com>
2025-03-20 14:47:22 +0900

    [FEAT] Item addItem 추가

--- side/src/main/java/com/server/side/item/service/impl/ItemServiceImpl.java
+++ side/src/main/java/com/server/side/item/service/impl/ItemServiceImpl.java
@@ -1,4 +1,22 @@
 package com.server.side.item.service.impl;
 
-public class ItemServiceImpl {
+import com.server.side.item.domain.ItemRepository;
+import com.server.side.item.dto.ItemDto;
+import com.server.side.item.dto.ItemRegistrationRequest;
+import com.server.side.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.server.side.item.dto.ItemDto.fromEntity;
+
+@Service
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository repository;
+
+    @Override
+    public ItemDto addItem(ItemRegistrationRequest request) {
+        return fromEntity(repository.save(request.toEntity()));
+    }
 }
