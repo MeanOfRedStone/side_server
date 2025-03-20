ef10dc02d2c314d3d549cf67c193cf898a9b9fc8
meanofredstone <hongs7701@naver.com>
2025-03-21 00:17:33 +0900

    [ADD] createItem 추가

---
+++ side/src/main/java/com/server/side/item/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.server.side.item.controller;
+
+import com.server.side.item.dto.ItemDto;
+import com.server.side.item.dto.ItemRegistrationRequest;
+import com.server.side.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.server.side.item.dto.ItemDto.fromEntity;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService service;
+
+    @PostMapping
+    public ResponseEntity<ItemDto> createItem(@RequestBody ItemRegistrationRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(fromEntity(service.addItem(request)));
+    }
+}
