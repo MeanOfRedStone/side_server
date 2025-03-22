ec79a5f39a8da12db5e06047ea6a6e51249df7c4
meanofredstone <hongs7701@naver.com>
2025-03-22 16:41:06 +0900

    [TEST] createItemDetail 테스트
    저장하는 것과 같은 값을 반환하는지 확인

---
+++ side/src/test/java/com/server/side/itemDetail/controller/ItemDetailControllerTest.java
@@ -0,0 +1,106 @@
+package com.server.side.itemDetail.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.server.side.config.InstantTypeAdapter;
+import com.server.side.item.domain.Item;
+import com.server.side.itemDetail.dto.ItemDetailDTO;
+import com.server.side.itemDetail.dto.ItemDetailRegistrationRequest;
+import com.server.side.itemDetail.service.impl.ItemDetailServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.Instant;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@Slf4j
+@WebMvcTest(ItemDetailController.class)
+public class ItemDetailControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ItemDetailServiceImpl itemDetailService;
+
+    private static Gson gson;
+
+    @BeforeAll
+    public static void setUp() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                .create();
+    }
+
+    @Test
+    void createItemDetailThenReturnSameItemDetail() throws Exception{
+        Item item = Item.builder()
+                .id(1L)
+                .name("셔츠")
+                .price(1000)
+                .category("상의")
+                .image("셔츠.png")
+                .information("멋진 셔츠")
+                .measurement("L - 100")
+                .build();
+
+        ItemDetailRegistrationRequest request1 = ItemDetailRegistrationRequest.builder()
+                .item(item)
+                .option("L")
+                .quantity(100)
+                .build();
+        ItemDetailDTO dto1 = request1.toResponse();
+
+        given(itemDetailService.addItemDetail(eq(request1))).willReturn(request1.toEntity());
+
+        ResultActions actions = mockMvc.perform(
+                post("/items")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(request1))
+        );
+
+        actions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.item.id").value(dto1.getItem().getId()))
+                .andExpect(jsonPath("$.option").value(dto1.getOption()))
+                .andExpect(jsonPath("$.quantity").value(dto1.getQuantity()));
+
+        ItemDetailRegistrationRequest request2 = ItemDetailRegistrationRequest.builder()
+                .item(item)
+                .option("M")
+                .quantity(200)
+                .build();
+        ItemDetailDTO dto2 = request2.toResponse();
+
+        given(itemDetailService.addItemDetail(eq(request2))).willReturn(request2.toEntity());
+
+        ResultActions actions2 = mockMvc.perform(
+                post("/items")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(request2))
+        );
+
+        actions2
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.item.id").value(dto2.getItem().getId()))
+                .andExpect(jsonPath("$.option").value(dto2.getOption()))
+                .andExpect(jsonPath("$.quantity").value(dto2.getQuantity()));
+
+
+    }
+}
