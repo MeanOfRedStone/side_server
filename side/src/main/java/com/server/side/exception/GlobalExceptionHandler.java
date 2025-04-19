ff8cde4d01dbcd4d9cffcfa2dd65246f9c379ee8
meanofredstone <hongs7701@naver.com>
2025-04-19 15:00:25 +0900

    [ADD] GlobalExceptionHandler 에 FileValidationException 추가 및 RestControllerAdvice로 전환

--- side/src/main/java/com/server/side/exception/GlobalExceptionHandler.java
+++ side/src/main/java/com/server/side/exception/GlobalExceptionHandler.java
@@ -1,17 +1,29 @@
 package com.server.side.exception;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.Locale;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(FileStorageException.class)
     public ResponseEntity<String> handleFileStorageException(FileStorageException ex) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("이미지 파일 저장 중 오류 발생" + ex.getMessage());
+    }
+
+    @ExceptionHandler(FileValidationException.class)
+    public String handleFileValidationException(FileValidationException ex, Locale locale) {
+        return messageSource.getMessage(ex.getMessage(), null, locale);
     }
 }
