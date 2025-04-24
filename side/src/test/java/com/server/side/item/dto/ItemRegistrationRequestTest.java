1b638eac9df19d2383b00bf2fe13dc3b47b95c00
meanofredstone <hongs7701@naver.com>
2025-04-24 21:12:37 +0900

    [ADD] description Valid 조건(NotBlank) 추가 및 관련 메시지 추가

--- side/src/test/java/com/server/side/item/dto/ItemRegistrationRequestTest.java
+++ side/src/test/java/com/server/side/item/dto/ItemRegistrationRequestTest.java
@@ -18,7 +18,8 @@ public class ItemRegistrationRequestTest {
         ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
                 .name(null)
                 .price(1000)
-                .category("상의").build();
+                .category("상의")
+                .description("멋진 셔츠").build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations1 = validator.validate(request1);
 
@@ -29,7 +30,8 @@ public class ItemRegistrationRequestTest {
         ItemRegistrationRequest request2 = ItemRegistrationRequest.builder()
                 .name("")
                 .price(1000)
-                .category("상의").build();
+                .category("상의")
+                .description("멋진 셔츠").build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations2 = validator.validate(request2);
 
@@ -39,7 +41,8 @@ public class ItemRegistrationRequestTest {
         ItemRegistrationRequest request3 = ItemRegistrationRequest.builder()
                 .name(" ")
                 .price(1000)
-                .category("상의").build();
+                .category("상의")
+                .description("멋진 셔츠").build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations3 = validator.validate(request3);
 
@@ -52,7 +55,8 @@ public class ItemRegistrationRequestTest {
         ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
                 .name("셔츠")
                 .price(null)
-                .category("상의").build();
+                .category("상의")
+                .description("멋진 셔츠").build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations1 = validator.validate(request1);
 
@@ -64,7 +68,8 @@ public class ItemRegistrationRequestTest {
         ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
                 .name("셔츠")
                 .price(-1)
-                .category("상의").build();
+                .category("상의")
+                .description("멋진 셔츠").build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations1 = validator.validate(request1);
 
@@ -78,6 +83,7 @@ public class ItemRegistrationRequestTest {
                 .name("셔츠")
                 .price(0)
                 .category(null)
+                .description("멋진 셔츠")
                 .build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations1 = validator.validate(request1);
@@ -89,6 +95,7 @@ public class ItemRegistrationRequestTest {
                 .name("셔츠")
                 .price(0)
                 .category("")
+                .description("멋진 셔츠")
                 .build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations2 = validator.validate(request2);
@@ -100,11 +107,51 @@ public class ItemRegistrationRequestTest {
                 .name("셔츠")
                 .price(0)
                 .category(" ")
+                .description("멋진 셔츠")
                 .build();
 
         Set<ConstraintViolation<ItemRegistrationRequest>> violations3 = validator.validate(request3);
 
         assertThat(violations3).anyMatch(v -> v.getPropertyPath().toString().equals("category")
                 && v.getMessage().equals("{item.category.notblank}"));
+    }
+
+    @Test
+    void shouldFailWhenDescriptionIsBlank() {
+        ItemRegistrationRequest request1 = ItemRegistrationRequest.builder()
+                .name("셔츠")
+                .price(0)
+                .category("상의")
+                .description(null)
+                .build();
+
+        Set<ConstraintViolation<ItemRegistrationRequest>> violations1 = validator.validate(request1);
+
+        assertThat(violations1).anyMatch(v -> v.getPropertyPath().toString().equals("description")
+        && v.getMessage().equals("{item.description.notblank}"));
+
+        ItemRegistrationRequest request2 = ItemRegistrationRequest.builder()
+                .name("셔츠")
+                .price(0)
+                .category("상의")
+                .description("")
+                .build();
+
+        Set<ConstraintViolation<ItemRegistrationRequest>> violations2 = validator.validate(request2);
+
+        assertThat(violations2).anyMatch(v -> v.getPropertyPath().toString().equals("description")
+                && v.getMessage().equals("{item.description.notblank}"));
+
+        ItemRegistrationRequest request3 = ItemRegistrationRequest.builder()
+                .name("셔츠")
+                .price(0)
+                .category("상의")
+                .description(" ")
+                .build();
+
+        Set<ConstraintViolation<ItemRegistrationRequest>> violations3 = validator.validate(request3);
+
+        assertThat(violations3).anyMatch(v -> v.getPropertyPath().toString().equals("description")
+                && v.getMessage().equals("{item.description.notblank}"));
     }
 }
