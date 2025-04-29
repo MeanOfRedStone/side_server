06699c07d306e99b2fb51f67bd16d7df2166598c
meanofredstone <hongs7701@naver.com>
2025-04-30 01:29:17 +0900

    [ADD]ItemDetailRegistrationRequest의 quantity 필드 @Min 어노테이션 추가

--- side/src/main/java/com/server/side/item/dto/ItemDetailRegistrationRequest.java
+++ side/src/main/java/com/server/side/item/dto/ItemDetailRegistrationRequest.java
@@ -2,6 +2,7 @@ package com.server.side.item.dto;
 
 import com.server.side.item.domain.Item;
 import com.server.side.item.domain.ItemDetail;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -17,6 +18,7 @@ public class ItemDetailRegistrationRequest {
     @NotBlank(message = "{itemDetail.option.notblank}")
     private String option;
     @NotNull(message = "{itemDetail.quantity.notnull}")
+    @Min(value = 0, message = "{itemDetail.quantity.min}")
     private Integer quantity;
 
     public ItemDetail toEntity() {
