4d650f3879d35ebcc19b4885b11b93a566bd76c8
meanofredstone <hongs7701@naver.com>
2025-05-01 22:19:10 +0900

    [REFACTOR]Item과 ItemsDetail을 함께 관리함에 따라 관련 DTO 수정
    1. 객체명
    Item.. -> ItemWithDetails
    
    2. 필드 추가
    List<ItemDetail> itemDetails

--- side/src/main/java/com/server/side/item/dto/ItemWithDetailsDTO.java
+++ side/src/main/java/com/server/side/item/dto/ItemWithDetailsDTO.java
@@ -1,16 +1,18 @@
 package com.server.side.item.dto;
 
 import com.server.side.item.domain.Item;
+import com.server.side.item.domain.ItemDetail;
 import lombok.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
-public class ItemDto {
+public class ItemWithDetailsDTO {
 
     private long id;
 
@@ -26,8 +28,16 @@ public class ItemDto {
 
     private List<String> information;
 
-    public static ItemDto fromEntity(Item item) {
-        return ItemDto.builder()
+    private List<ItemDetailDTO> itemDetails;
+
+    public static ItemWithDetailsDTO fromEntities(Item item, List<ItemDetail> itemDetails) {
+        List<ItemDetailDTO> itemDetailDTOS = itemDetails.stream()
+                .map(itemDetail -> ItemDetailDTO.builder()
+                        .option(itemDetail.getOption())
+                        .quantity(itemDetail.getQuantity())
+                        .build())
+                .collect(Collectors.toList());
+        return ItemWithDetailsDTO.builder()
                 .id(item.getId())
                 .name(item.getName())
                 .price(item.getPrice())
@@ -35,6 +45,7 @@ public class ItemDto {
                 .description(item.getDescription())
                 .image(item.getImage())
                 .information(item.getInformation())
+                .itemDetails(itemDetailDTOS)
                 .build();
     }
 
---
+++ side/src/main/java/com/server/side/item/dto/ItemWithDetailsRegistrationRequest.java
@@ -0,0 +1,57 @@
+package com.server.side.item.dto;
+
+import com.server.side.item.domain.Item;
+import com.server.side.item.domain.ItemDetail;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class ItemWithDetailsRegistrationRequest {
+
+    @NotBlank(message = "{item.name.notblank}")
+    private String name;
+    @NotNull(message = "{item.price.notnull}")
+    @Min(value = 0, message = "{item.price.min}")
+    private Integer price;
+    @NotBlank(message = "{item.category.notblank}")
+    private String category;
+    @NotBlank(message = "{item.description.notblank}")
+    private String description;
+
+    private String image;
+
+    private List<String> information;
+    @NotNull(message = "{item.itemDetails.notnull}")
+    private List<ItemDetailRegistrationRequest> itemDetails;
+
+    public Item toItemEntity() {
+        return Item.builder()
+                .name(this.name)
+                .price(this.price)
+                .category(this.category)
+                .description(this.description)
+                .image(this.image)
+                .information(this.information)
+                .build();
+    }
+
+    public List<ItemDetail> toItemDetailEntity() {
+        return itemDetails.stream()
+                .map(itemDetail -> ItemDetail.builder()
+                        .item(toItemEntity())
+                        .option(itemDetail.getOption())
+                        .quantity(itemDetail.getQuantity())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+}
