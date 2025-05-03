35c630a47e305af59b730e400a882d8a8a2cef1e
meanofredstone <hongs7701@naver.com>
2025-05-04 00:16:03 +0900

    [ADD]ItemWithDetailsDTO 수정
    1. 스트림문 빌드패턴내로 이동
    
    2. ItemDTO 생성에 따라 ItemWithDetailsDTO -> ItemDTO 변환 메소드 생성

--- side/src/main/java/com/server/side/item/dto/ItemWithDetailsDTO.java
+++ side/src/main/java/com/server/side/item/dto/ItemWithDetailsDTO.java
@@ -31,12 +31,6 @@ public class ItemWithDetailsDTO {
     private List<ItemDetailDTO> itemDetails;
 
     public static ItemWithDetailsDTO fromEntities(Item item, List<ItemDetail> itemDetails) {
-        List<ItemDetailDTO> itemDetailDTOS = itemDetails.stream()
-                .map(itemDetail -> ItemDetailDTO.builder()
-                        .option(itemDetail.getOption())
-                        .quantity(itemDetail.getQuantity())
-                        .build())
-                .collect(Collectors.toList());
         return ItemWithDetailsDTO.builder()
                 .id(item.getId())
                 .name(item.getName())
@@ -45,8 +39,24 @@ public class ItemWithDetailsDTO {
                 .description(item.getDescription())
                 .image(item.getImage())
                 .information(item.getInformation())
-                .itemDetails(itemDetailDTOS)
+                .itemDetails(itemDetails.stream()
+                        .map(itemDetail -> ItemDetailDTO.builder()
+                                .option(itemDetail.getOption())
+                                .quantity(itemDetail.getQuantity())
+                                .build())
+                        .collect(Collectors.toList()))
                 .build();
     }
 
+    public ItemDTO toItemDTO() {
+        return ItemDTO.builder()
+                .id(this.getId())
+                .name(this.getName())
+                .price(this.getPrice())
+                .category(this.getCategory())
+                .description(this.getDescription())
+                .image(this.getImage())
+                .information(this.getInformation())
+                .build();
+    }
 }
